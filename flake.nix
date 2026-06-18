bc9d3447c95399425966ffc0cc0edba468af1d09
Marcel Scherzinger <>
2026-06-18 13:22:24 +0200

    feat(nix): add builder function

--- flake.nix
+++ flake.nix
@@ -3,25 +3,46 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    scratch-test-koin2627 = {
+      url = "github:marcel-scherzinger/scratch-test-koin2627";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
+    scratch-test-koin2627,
+    flake-utils,
   }: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
-  in {
-    # devShells."x86_64-linux".default = import ./shell.nix pkgs;
+  in
+    flake-utils.lib.eachDefaultSystem (system: rec {
+      # devShells.default = import ./shell.nix pkgs;
 
-    packages."x86_64-linux".scratch-test-backend = pkgs.rustPlatform.buildRustPackage {
-      name = "scratch-test-backend";
-      src = ./.;
-      buildInputs = [];
-      nativeBuildInputs = [pkgs.pkg-config];
-      cargoHash = "sha256-v1ZJ536DIkWf5lQENxfu6EDaZqRdd2FRNLNrT5taQ0M=";
-    };
+      buildBackend = {
+        exercises ? scratch-test-koin2627,
+        cargoHash ? "sha256-5FujgZhRoYaAu1m//aDwlt2iIwhBjiYp5B5VLeUn+1M=",
+      }:
+        pkgs.rustPlatform.buildRustPackage {
+          name = "scratch-test-backend";
+          src = ./.;
+          buildInputs = [];
+          nativeBuildInputs = [pkgs.pkg-config];
+          cargoHash = cargoHash;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.scratch-test-backend;
-  };
+          postPatch = ''
+            echo "Replace url with local files..., if this fails: compare flake.nix with Cargo.toml url"
+            grep 'git = "https://github.com/Marcel-Scherzinger/scratch-test-koin2627"' Cargo.toml
+            substituteInPlace Cargo.toml \
+              --replace 'git = "https://github.com/Marcel-Scherzinger/scratch-test-koin2627"' \
+              'path = "${exercises}"'
+          '';
+        };
+
+      packages.scratch-test-backend = buildBackend {};
+
+      packages.default = self.packages.x86_64-linux.scratch-test-backend;
+    });
 }
-
